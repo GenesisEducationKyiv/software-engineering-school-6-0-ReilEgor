70532819843c47688c96a5532e10c2cdc19be414
YehorReil <egorrejl44@gmail.com>
2026-05-20 22:41:01 +0300

    ci: fix e2e tests

--- static/e2e/register.spec.js
+++ static/e2e/register.spec.js
@@ -2,6 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('register', async ({ page }) => {
     await page.goto('http://localhost:8080/');
+
     await page.getByRole('textbox', { name: 'Enter your API key' }).click();
     await page.getByRole('textbox', { name: 'Enter your API key' }).fill('my-super-secret-token-123');
     await page.getByRole('textbox', { name: 'alex@example.com' }).click();
@@ -9,5 +10,15 @@ test('register', async ({ page }) => {
     await page.getByRole('textbox', { name: 'golang/go' }).click();
     await page.getByRole('textbox', { name: 'golang/go' }).fill('golang-migrate/migrate');
     await page.getByRole('button', { name: 'Subscribe' }).click();
+
+    const message = page.locator('#message');
+    await expect(message).toBeVisible();
+    await expect(message).toHaveClass(/bg-green-50/);
+    await expect(page.locator('#repository')).toBeEmpty();
+
     await page.getByRole('button', { name: 'My Subs' }).click();
+    const subList = page.locator('#subList');
+    await expect(subList).toBeVisible();
+    const listItems = page.locator('#listItems');
+    await expect(listItems).toContainText('golang-migrate/migrate');
 });
