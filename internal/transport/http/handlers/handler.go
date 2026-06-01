56555e82b6c4ae22fe913e36a3f25d2bcaa2b34d
YehorReil <egorrejl44@gmail.com>
2026-06-01 16:20:26 +0300

    fix: gci issue

--- internal/transport/http/handlers/handler.go
+++ internal/transport/http/handlers/handler.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 
